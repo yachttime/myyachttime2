eaa9fce9d7bc257294c1ba402f894978ac8572b5
yachttime <jeff@azmarine.net>
2026-01-30 15:34:11 -0700

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import { ErrorBoundary } from './components/ErrorBoundary.tsx';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-      <SpeedInsights />
-    </ErrorBoundary>
+    <App />
+    <SpeedInsights />
   </StrictMode>
 );
