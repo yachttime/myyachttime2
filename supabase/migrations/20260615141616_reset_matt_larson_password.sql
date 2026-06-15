4a8b6ad2b3836d677e3774a7e38d9b0c21dd775c
yachttime <jeff@azmarine.net>
2026-06-15 07:16:39 -0700

    Added 20260615141616_reset_matt_larson_password.sql

---
+++ supabase/migrations/20260615141616_reset_matt_larson_password.sql
@@ -0,0 +1,3 @@
+
+SELECT update_user_password('54596742-2c1b-466c-946e-e3df75522a25', 'AZMarine2026!');
+UPDATE user_profiles SET must_change_password = false WHERE user_id = '54596742-2c1b-466c-946e-e3df75522a25';
