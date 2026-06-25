262ebbe6e2e8873c465bf2c48832b6bc29b3bc53
yachttime <jeff@azmarine.net>
2026-06-25 11:26:43 -0700

    Added 20260625182552_revert_melody_trip3_hours.sql

---
+++ supabase/migrations/20260625182615_fix_melody_trip3_engine_gen_swap.sql
@@ -0,0 +1,7 @@
+
+UPDATE trip_inspections
+SET port_engine_hours = 8486.0,
+    stbd_engine_hours = 4447.0,
+    port_gen_hours    = 4447.0,
+    stbd_gen_hours    = 656.0
+WHERE id = 'd1050c6b-01b2-46a6-b10e-515bb3755644';
