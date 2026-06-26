8d348b339476c1b18cf506629496497d93d3ccfa
yachttime <jeff@azmarine.net>
2026-06-26 10:39:29 -0700

    Added 20260626173902_fix_matt_larson_trip4_end_hours_match_start.sql

---
+++ supabase/migrations/20260626173902_fix_matt_larson_trip4_end_hours_match_start.sql
@@ -0,0 +1,6 @@
+UPDATE trip_inspections
+SET port_engine_hours = 849.0,
+    stbd_engine_hours = 850.0,
+    port_gen_hours = 6745.0,
+    stbd_gen_hours = 6573.0
+WHERE id = '2d5a9ed5-8d5f-4463-a60c-446144162891';
