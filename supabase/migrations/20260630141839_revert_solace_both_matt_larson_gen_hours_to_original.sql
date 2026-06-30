72a602ab948d0788ed9ddf2053779a80a44cfc87
yachttime <jeff@azmarine.net>
2026-06-30 07:19:05 -0700

    Added 20260630141839_revert_solace_both_matt_larson_gen_hours_to_original.sql

---
+++ supabase/migrations/20260630141839_revert_solace_both_matt_larson_gen_hours_to_original.sql
@@ -0,0 +1,8 @@
+
+UPDATE trip_inspections
+SET port_gen_hours = '6745.0', stbd_gen_hours = '6573.0'
+WHERE id = '2d5a9ed5-8d5f-4463-a60c-446144162891';
+
+UPDATE trip_inspections
+SET port_gen_hours = '6635.0', stbd_gen_hours = '6919.0'
+WHERE id = '9f4fbcd4-7448-45a4-a0c8-25598ad4b0f6';
